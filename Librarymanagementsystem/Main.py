e7f0673d2aef02a9953d4ee42dcdb35276591045
Hustlingshreejan <hustlingshreejan@gmail.com>
2020-07-19 11:04:38 +0545

    change in main function, added dictionary as an instance variable and dictionary can be saved

--- Librarymanagementsystem/Main.py
+++ Librarymanagementsystem/Main.py
@@ -2,8 +2,12 @@
 class Library:
 
     def __init__(self,libraryname,booklist):
+        self.bookdata={}
         self.libarayname=libraryname
         self.booklist= booklist
+
+        for book in booklist:
+            self.bookdata[book] = None
 
     def listofbook(self):
         listt=self.booklist
@@ -29,29 +33,28 @@ class Library:
                 break
 
     def lendbook(self):
-        dictt={"Nepali":"Suresh"}
+
         print("Which book ???")
         userlend=input("Book name:").capitalize()
         if userlend in self.booklist:
             print("Checking the availability of the book...\n")
-            for keys in dictt.keys():
-                if userlend == keys:
-                    print(f"This book has already been taken by {dictt.get(userlend)}.\nIt will be available in thisthis date...\nReserver for me (option)")
-                    # print("Do you want to take any other book?")
-                    break
-            else:
+
+            if self.bookdata[userlend] is None:
                 print(f"The book '{userlend}', you requested is currently available.\n")
                 print("Do you want to lend it: ")
                 lendingpermission = int(input("1.yes 2.No"))
                 if lendingpermission == 1:
                     username = input("Your name:")
-                    dictt.update({userlend:username})
+                    self.bookdata[userlend]=username
                     print("Your name and the name of the book has been registered successfully\n")
                     print("List of the book with the name of student who took the book".upper())
-                    for key, val in dictt.items():
-                        print(f"Book: {key} -- Student's name: {val}")
-                else:
-                    pass
+                    for key, val in self.bookdata.items() :
+                        if val!=None:
+                            print(f"Book: {key} -- Student's name: {val}")
+            else:
+                print(f"This book has already been taken by {self.bookdata.get(userlend)}.\nIt will be available in thisthis date...\nReserver for me (option)")
+
+
         else:
             print(f"Sorry there is no book named as '{userlend}' in our library. Please type the spelling of the book correctly.")
 
@@ -65,26 +68,37 @@ class Library:
         else:
             print("hahahah")
 
+def main():
+    listofbooks=["Math", "Science","Nepali"]
+    libraryname="Shreejan's library"
+    shreelibrary=Library(libraryname, listofbooks)
 
-shreelibrary=Library("Shreejan's library", ["Math", "Science","Nepali"])
-if __name__ == '__main__':
+    print('Do you want to\n 1:see the detail of the books\n 2:Lend of book \n 3:donate a book \n 4:return a book\n Q for Exit from the system ')
 
-        print("\nDo you want to\n"
-              " 1:see the detail of the books\n "
-              "2:Lend of book \n"
-              " 3:donate a book \n"
-              " 4:return a book".upper())
-        firstuser=int(input("Provide the option to the system:"))
-        if firstuser==1:
+
+    Exit = False
+    while (Exit is not True):
+        firstuser = input("\noption:").capitalize()
+
+        if firstuser == '1':
             shreelibrary.listofbook()
-        elif firstuser==2:
+        elif firstuser == '2':
             shreelibrary.lendbook()
-        elif firstuser==3:
+        elif firstuser == '3':
             shreelibrary.addbook()
-        elif firstuser==4:
+        elif firstuser == '4':
             shreelibrary.returnbook()
+        elif firstuser == 'Q':
+            Exit = True
         else:
             print(f"Welcome to {shreelibrary.libarayname} ")
+
+
+
+
+if __name__ == '__main__':
+    main()
+
 
 
 
